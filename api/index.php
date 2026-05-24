2fdfae7504177589a48b6e5c9b97b173e624d6f5
Wardin <wardinhalawa1@gmail.com>
2026-05-24 13:06:43 +0700

    fix konfigurasi server vercel

---
+++ api/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Mengarahkan Vercel untuk membaca autoload dan app dari folder utama
+require __DIR__ . '/../bootstrap/app.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+$kernel->terminate($request, $response);
